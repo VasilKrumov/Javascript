ce95e089f3d4962623387dc98c2950bac3f6f67c
Vasil Krumov <vassil.krumov@gmail.com>
2020-11-06 00:08:06 +0200

    :sparkles: While Loop Lab

---
+++ JS Basics/5. While-Loops/While-Loops - Lab/_08_graduation.js
@@ -0,0 +1,28 @@
+function graduation(arr) {
+    let name = arr[0]
+    let index = 1
+    let average = 0
+    let sum = 0
+    let grade = 1
+    let repeat = 0
+
+    while (index < arr.length) {
+        arr[index] = Number(arr[index])
+        if (arr[index] < 4) {
+            index++
+            repeat += 1
+            if (repeat === 2) {
+                console.log(`${name} has been excluded at ${grade} grade`)
+                return
+            }
+            continue
+        } else {
+            grade++
+            sum += arr[index]
+            index++
+        }
+    }
+
+    average = sum / 12
+    console.log(`${name} graduated. Average grade: ${average.toFixed(2)}`)
+}
